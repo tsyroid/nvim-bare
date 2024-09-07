13ffb6d9d6f1b17671959a6646d5622d20d93969
Tom Syroid <tsyroid@gmail.com>
2024-09-07 09:48:27 -0700

    Add plugins/autopairs.lua

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,13 @@
+-- nvim/lua/nvim/lua/nvim/lua/nvim/lua/nvim/lua/nvim/lua/nvim/lua/nvim/lua/plugins/autopairs.lua
+-- https://github.com/windwp/nvim-autopairs
+
+-- TODO: Disable rule for "```"
+
+return {
+  "windwp/nvim-autopairs",
+  enabled = true,
+  event = "InsertEnter",
+  config = true,
+  -- use opts = {} for passing setup options
+  -- equivalent to setup({}) function
+}
